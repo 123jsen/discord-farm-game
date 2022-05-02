10b93711a41515978bba2e7f6e4949cfd0ef4fa9
SenYanHo <123sendodo@gmail.com>
2022-05-02 12:16:55 +0800

    Implement basic create player function

--- src/create-player.js
+++ src/create-player.js
@@ -1,5 +1,20 @@
-async function createPlayer(interaction) {
+const Player = require("./models/player.model.js");
 
+async function createPlayer(interaction) {
+    const userId = interaction.user.id;
+    const player = await Player.findOne({ userId }).exec();
+
+    // No creation needed if player is already in DB
+    if (player) return;
+
+    Player.create({
+        userId,
+        money: 100,
+        farmWidth: 3,
+        farm: [],
+    });
+
+    console.log(`Created new player for ${interaction.user.username}`);
 }
 
 module.exports = createPlayer;
