b480e2aefd8a8b73434f234d4cf5a11e779b8cb5
SenYanHo <80093853+SenYanHo@users.noreply.github.com>
2022-05-04 01:34:16 +0800

    Create check command

---
+++ src/commands/check.command.js
@@ -0,0 +1,18 @@
+// TODO: make line 30 arrays of promises
+
+const { MessageEmbed } = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const Player = require('../models/player.model.js');
+const Crop = require("../models/crop.model.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('check')
+        .setDescription('Check the progress of crops'),
+    async execute(interaction) {
+        const userId = interaction.user.id;
+        const player = await Player.findOne({ userId }).populate('farm').exec();
+        const crops = player.farm;
+        const emptyCrop = await Crop.findOne({ name: "Empty" }).exec();
+    },
+};
