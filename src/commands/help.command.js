3357fd9ee57434faeaf8d77544d39b327ab1b783
SenYanHo <80093853+SenYanHo@users.noreply.github.com>
2022-05-06 01:22:15 +0800

    Updated help command embed

--- src/commands/help.command.js
+++ src/commands/help.command.js
@@ -14,8 +14,11 @@ module.exports = {
                 { name: 'How to Play', value: 'You plant crops in your field and wait for them to be mature. After that, you can harvest the crops and earn money. This allows you to plant more expensive crops and earn more money.\n\nAt first, each player starts with 100 gold and a 3x3 land. You can upgrade your farm using gold in the future.' },
                 { name: '/farm', value: 'Shows your current farm plot' },
                 { name: '/plant', value: 'Plant crop at target location' },
+                { name: '/plantall', value: 'Plant as many as possible crops' },
                 { name: '/check', value: 'Check when a crop is mature' },
-                { name: '/harvest', value: 'Harvest all mature crops and sell them' })
+                { name: '/harvest', value: 'Harvest all mature crops and sell them' },
+                { name: '/leaderboard', value: 'Show the top 5 richest player' },
+                { name: '/upgrade', value: 'Upgrade your farm' })
 
         await interaction.reply({ embeds: [helpEmbed] });
     },
