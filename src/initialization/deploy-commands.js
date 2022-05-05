3ee8468907c943d48001350239d5571ad60a85ad
SenYanHo <80093853+SenYanHo@users.noreply.github.com>
2022-05-05 15:59:04 +0800

    Reorganized initialization files

--- src/initialization/deploy-commands.js
+++ src/initialization/deploy-commands.js
@@ -7,7 +7,7 @@ const commands = [];
 const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
+	const command = require(`../commands/${file}`);
 	commands.push(command.data.toJSON());
 	console.log(`Registered command /${command.data.name}`);
 }
--- src/initialization/init-game.js
+++ src/initialization/init-game.js
@@ -1,9 +1,9 @@
 // Initialize crops list
 
-const Crop = require("./models/crop.model.js");
-const cropList = require("../data/crops.json");
+const Crop = require('../models/crop.model.js');
+const cropList = require('../data/crops.json');
 
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const dbUri = process.env.MONGODB_URI;
@@ -11,13 +11,13 @@ mongoose.connect(dbUri);
 
 const db = mongoose.connection;
 
-db.on("error", console.error.bind(console, "connection to mongodb error"));
+db.on('error', console.error.bind(console, 'connection to mongodb error'));
 
-db.once("open", async () => {
-    console.log("Connection to mongoDB server Success");
+db.once('open', async () => {
+    console.log('Connection to mongoDB server Success');
 
     await Crop.deleteMany({});
-    console.log("Deleted all crops");
+    console.log('Deleted all crops');
 
     const promises = [];
 
