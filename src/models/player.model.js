f31f5d171f7987acf0e1c3f2c1429097f471c473
SenYanHo <80093853+SenYanHo@users.noreply.github.com>
2022-05-08 18:42:23 +0800

    Reworked Schema for 2.0

--- src/models/player.model.js
+++ src/models/player.model.js
@@ -3,11 +3,22 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const PlayerSchema = Schema({
+    // User Data
     userId: { type: String, required: true, unique: true },
-    money: { type: Number, required: true },
-    farmWidth: { type: Number, required: true },
-    farm: [{ type: Schema.Types.ObjectId, ref: "Crop" }],
-    timer: [{ type: Date }]
+    farmName: { type: String, required: true, default: 'Farm' },
+
+    // Currency
+    money: { type: Number, required: true, default: 0 },
+    wood: { type: Number, required: true, default: 0 },
+    stone: { type: Number, required: true, default: 0 },
+
+    // Plot Detail
+    farmWidth: { type: Number, required: true, default: 3 },
+    farm: [{ type: String }],
+    timer: [{ type: Date, default: null }],
+
+    // Building Detail
+    building: [{ type: String }]
 });
 
 module.exports = mongoose.model("Player", PlayerSchema);
