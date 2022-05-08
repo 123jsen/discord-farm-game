7beec014749280e5421bdbde7dacb916ff7c213c
SenYanHo <80093853+SenYanHo@users.noreply.github.com>
2022-05-08 18:42:47 +0800

    Reworked export to more structured system

---
+++ data/export.base.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
+function exportData(folderPath) {
+    const dataFiles = fs.readdirSync(folderPath).filter(file => file.endsWith('.json'));
+    const data = [];
+
+    for (const file of dataFiles) {
+        const singleton = require(`${folderPath}/${file}`);
+
+        // Unpack array before pushing
+        if (Array.isArray(singleton))
+            data.push(...singleton);
+        else
+            data.push(singleton)
+
+        console.log(`pushed ${singleton} from ${folderPath}/${file}`);
+    }
+
+    console.log(data);
+    return data;
+}
+
+
+module.exports = exportData;
